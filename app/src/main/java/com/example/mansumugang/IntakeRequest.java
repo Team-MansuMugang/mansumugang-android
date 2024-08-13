88d4abada70e7c28ede4fb47680a9d3a00d4aeb3
KIMHEEJIN <gmlwls768@naver.com>
2024-08-13 20:15:26 +0900

    약,병원 토클 수정

--- app/src/main/java/com/example/mansumugang/IntakeRequest.java
+++ app/src/main/java/com/example/mansumugang/IntakeRequest.java
@@ -5,52 +5,78 @@ import java.util.List;
 
 public class IntakeRequest {
 
-    @SerializedName("medicineIds")
-    private List<Long> medicineIds;
-
     @SerializedName("hospitalId")
     private Long hospitalId;
 
-    @SerializedName("medicineIntakeTime")
-    private String medicineIntakeTime;
-
-    @SerializedName("scheduledMedicineIntakeDate")
-    private String scheduledMedicineIntakeDate;
-
-
+    @SerializedName("medicine")
+    private Medicine medicine;
 
     // Constructors
-    public IntakeRequest(Long hospitalId){
-        this.hospitalId = hospitalId;
-    }
     public IntakeRequest(List<Long> medicineIds, String medicineIntakeTime, String scheduledMedicineIntakeDate) {
-        this.medicineIds = medicineIds;
-        this.medicineIntakeTime = medicineIntakeTime;
-        this.scheduledMedicineIntakeDate = scheduledMedicineIntakeDate;
+        this.medicine = new Medicine(medicineIds, medicineIntakeTime, scheduledMedicineIntakeDate);
+    }
+
+    public IntakeRequest(Long hospitalId) {
+        this.hospitalId = hospitalId;
     }
 
     // Getters and Setters
-    public List<Long> getMedicineIds() {
-        return medicineIds;
+    public Long getHospitalId() {
+        return hospitalId;
     }
 
-    public void setMedicineIds(List<Long> medicineIds) {
-        this.medicineIds = medicineIds;
+    public void setHospitalId(Long hospitalId) {
+        this.hospitalId = hospitalId;
     }
 
-    public String getMedicineIntakeTime() {
-        return medicineIntakeTime;
+    public Medicine getMedicine() {
+        return medicine;
     }
 
-    public void setMedicineIntakeTime(String medicineIntakeTime) {
-        this.medicineIntakeTime = medicineIntakeTime;
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
     }
 
-    public String getScheduledMedicineIntakeDate() {
-        return scheduledMedicineIntakeDate;
-    }
+    public static class Medicine {
+        @SerializedName("medicineIds")
+        private List<Long> medicineIds;
 
-    public void setScheduledMedicineIntakeDate(String scheduledMedicineIntakeDate) {
-        this.scheduledMedicineIntakeDate = scheduledMedicineIntakeDate;
+        @SerializedName("medicineIntakeTime")
+        private String medicineIntakeTime;
+
+        @SerializedName("scheduledMedicineIntakeDate")
+        private String scheduledMedicineIntakeDate;
+
+        // Constructors
+        public Medicine(List<Long> medicineIds, String medicineIntakeTime, String scheduledMedicineIntakeDate) {
+            this.medicineIds = medicineIds;
+            this.medicineIntakeTime = medicineIntakeTime;
+            this.scheduledMedicineIntakeDate = scheduledMedicineIntakeDate;
+        }
+
+        // Getters and Setters
+        public List<Long> getMedicineIds() {
+            return medicineIds;
+        }
+
+        public void setMedicineIds(List<Long> medicineIds) {
+            this.medicineIds = medicineIds;
+        }
+
+        public String getMedicineIntakeTime() {
+            return medicineIntakeTime;
+        }
+
+        public void setMedicineIntakeTime(String medicineIntakeTime) {
+            this.medicineIntakeTime = medicineIntakeTime;
+        }
+
+        public String getScheduledMedicineIntakeDate() {
+            return scheduledMedicineIntakeDate;
+        }
+
+        public void setScheduledMedicineIntakeDate(String scheduledMedicineIntakeDate) {
+            this.scheduledMedicineIntakeDate = scheduledMedicineIntakeDate;
+        }
     }
 }
