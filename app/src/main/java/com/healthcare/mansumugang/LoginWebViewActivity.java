89c4eaa16172bf17338d8348d831be8444682647
Heejin Kim <gmlwls768@naver.com>
2024-11-03 14:29:26 +0900

    fix: webview 접근제한

--- app/src/main/java/com/healthcare/mansumugang/LoginWebViewActivity.java
+++ app/src/main/java/com/healthcare/mansumugang/LoginWebViewActivity.java
@@ -1,5 +1,4 @@
 package com.healthcare.mansumugang;
-
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,37 +9,29 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginWebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private static final String URL_MAIN = Constants.MAIN_URL + "/sign-up/patient";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        // WebView를 초기화하고 JavaScript를 사용할 수 있도록 설정합니다.
         webView = findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        String url = Constants.MAIN_URL + "/sign-up/patient";
-        // WebViewClient를 설정하여 페이지 로딩 및 URL 변경을 처리합니다.
         webView.setWebViewClient(new WebViewClient() {
-
             @Override
             public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
                 super.doUpdateVisitedHistory(view, url, isReload);
 
-                // 방문 기록이 업데이트될 때 호출되며, URL이 변경되었는지 확인합니다.
-                // 현재 URL이 가입 완료 페이지와 다를 경우, 앱을 종료하고 결과를 반환합니다.
-                if (!url.startsWith(url)) {
+                // URL이 메인 URL에서 벗어날 경우 액티비티 종료
+                if (!url.startsWith(URL_MAIN)) {
                     setResult(RESULT_OK);
                     finish();
                 }
             }
         });
 
-        // WebViewClient 설정하여 웹 페이지의 웹 관련 이벤트를 처리합니다.
-        webView.setWebViewClient(new WebViewClient());
-
-        // 웹뷰에서 로드할 URL을 지정하고 페이지를 로드합니다.
-        webView.loadUrl(url);
+        webView.loadUrl(URL_MAIN);
     }
 }
--- app/src/main/java/com/healthcare/mansumugang/WithdrawWebViewActivity.java
+++ app/src/main/java/com/healthcare/mansumugang/WithdrawWebViewActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class WithdrawWebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private static final String URL_BASE = Constants.MAIN_URL + "/account/withdraw-patient";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,51 +29,66 @@ public class WithdrawWebViewActivity extends AppCompatActivity {
         webSettings.setBuiltInZoomControls(true);
         webView.requestFocusFromTouch();
 
-
         // 웹뷰가 크로스 도메인 쿠키, 스토리지 등을 지원하도록 설정
         webSettings.setDomStorageEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
-        String url = Constants.MAIN_URL + "/account/withdraw-patient";
-        System.out.println(url);
+        // WebViewClient 설정
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
                 super.doUpdateVisitedHistory(view, url, isReload);
-                if (!url.startsWith(Constants.MAIN_URL + "/account/withdraw-patient")) {
+
+                // URL이 메인 URL에서 벗어날 경우 액티비티 종료
+                if (!url.startsWith(URL_BASE)) {
                     setResult(RESULT_OK);
                     finish();
                 }
             }
         });
 
+        // WebChromeClient 설정
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-                new AlertDialog.Builder(view.getContext()).setTitle("알림").setMessage(message).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                }).setCancelable(false).create().show();
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
                 return true;
             }
 
             @Override
             public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
-                new AlertDialog.Builder(view.getContext()).setTitle("알림").setMessage(message).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.cancel();
-                    }
-                }).setCancelable(false).create().show();
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.cancel();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
                 return true;
             }
         });
 
-        webView.loadUrl(url);
+        // URL 로드
+        webView.loadUrl(URL_BASE);
     }
 }
